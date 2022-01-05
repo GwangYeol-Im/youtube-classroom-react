41363eef3ec943f34ddd00ed8e15424cd67f0a8b
imgwang-yeol <mkitigy@gmail.com>
2022-01-05 16:38:57 +0900

    chore: videos json 파일 추가

---
+++ src/assets/json/videos.ts
@@ -0,0 +1,356 @@
+const VIDEOS = [
+  {
+    kind: 'youtube#searchResult',
+    etag: '3maksvlVnarhUScX-uST5bxsalY',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'rSD7jb4Xjq8',
+    },
+    snippet: {
+      publishedAt: '2019-12-24T19:00:05Z',
+      channelId: 'UCuFPaemAaMR8R5cHzjy23dQ',
+      title: 'Snow Man「D.D.」MV (YouTube ver.)',
+      description:
+        '2020.01.22 Debut ！ Snow Man vs SixTONES「D.D. / Imitation Rain」 ----淘汰の先にある未来へ---- 近未来的な世界観、舞い踊るSnow ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/rSD7jb4Xjq8/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/rSD7jb4Xjq8/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/rSD7jb4Xjq8/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Snow Man',
+      liveBroadcastContent: 'none',
+      publishTime: '2019-12-24T19:00:05Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: '0UTo18paMWm72wWTOyc5yoMgnAI',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'BtyHYIpykN0',
+    },
+    snippet: {
+      publishedAt: '2019-03-22T19:00:01Z',
+      channelId: 'UCtBjt766LG9EDVKZx4Q4IHw',
+      title: 'PNL - Au DD [Clip Officiel]',
+      description:
+        '« Au DD », troisième extrait du nouvel album de PNL "Deux Frères", disponible maintenant en deux versions physiques ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/BtyHYIpykN0/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/BtyHYIpykN0/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/BtyHYIpykN0/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'PNL',
+      liveBroadcastContent: 'none',
+      publishTime: '2019-03-22T19:00:01Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'v1T5jnAVZv2oQvU3Cv-y3cm04do',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'UT0EiORz_LQ',
+    },
+    snippet: {
+      publishedAt: '2018-07-26T02:27:43Z',
+      channelId: 'UClYV6hHlupm_S_ObS1W-DYw',
+      title: 'D.D.',
+      description:
+        'Provided to YouTube by Universal Music Group D.D. · The Weeknd Trilogy ℗ 2012 XO&co., Inc. Released on: 2012-11-13 ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/UT0EiORz_LQ/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/UT0EiORz_LQ/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/UT0EiORz_LQ/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'The Weeknd - Topic',
+      liveBroadcastContent: 'none',
+      publishTime: '2018-07-26T02:27:43Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'LMd6OpVvzn1Qk8mNhnHPXtVxxGc',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'o21gTD0wizw',
+    },
+    snippet: {
+      publishedAt: '2020-11-12T02:00:09Z',
+      channelId: 'UCACti7fagQ5OH_47IaWwpPQ',
+      title: '[온스테이지2.0] DD!(디디) - Dancing Under The Moonlight',
+      description:
+        '[아티스트 정보] ARTIST: DD!(디디) TITLE: Dancing Under The Moonlight 참여 세션: 이정윤(Drums), 유연아(E.B, ContraBass), ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/o21gTD0wizw/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/o21gTD0wizw/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/o21gTD0wizw/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: '온스테이지ONSTAGE',
+      liveBroadcastContent: 'none',
+      publishTime: '2020-11-12T02:00:09Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'w7jYFyb6qhBzlALHdYUMvEuqyQE',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'oiKW9l1s68Q',
+    },
+    snippet: {
+      publishedAt: '2021-06-08T03:00:12Z',
+      channelId: 'UCNS7BQ7gEJxTbEDsffsGqkg',
+      title:
+        '[MV] FRankly(프랭클리) - DD(Drunk Dreaming) / Official Music Video',
+      description:
+        'FRankly(프랭클리) - DD(Drunk Dreaming) / Official Music Video Melon ▷ https://bit.ly/3zkFuQG Genie ▷ https://bit.ly/3x5hor3 ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/oiKW9l1s68Q/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/oiKW9l1s68Q/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/oiKW9l1s68Q/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'POCLANOS',
+      liveBroadcastContent: 'none',
+      publishTime: '2021-06-08T03:00:12Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'Ta3RT4pFFFVSZljCK93HLwh1rkw',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'aWJqUqC0s6w',
+    },
+    snippet: {
+      publishedAt: '2019-12-31T16:00:02Z',
+      channelId: 'UCuFPaemAaMR8R5cHzjy23dQ',
+      title: 'Snow Man「D.D.」(dance ver.)',
+      description:
+        '2020.01.22 Debut ！ Snow Man vs SixTONES「D.D. / Imitation Rain」 ----淘汰の先にある未来へ---- 近未来的な世界観、舞い踊るSnow ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/aWJqUqC0s6w/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/aWJqUqC0s6w/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/aWJqUqC0s6w/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Snow Man',
+      liveBroadcastContent: 'none',
+      publishTime: '2019-12-31T16:00:02Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: '1dhAE-beQVfpzqF31zZLeVwVHpk',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'eE-APRkDBO0',
+    },
+    snippet: {
+      publishedAt: '2018-12-20T03:00:02Z',
+      channelId: 'UCmLiSrat4HW2k07ahKEJo4w',
+      title: '어린 시절 가장 사랑스러운 고양이 디디',
+      description:
+        '어릴 때 정말 주먹만 했던 고양이가 지금은 엄청 거대해 졌어요. 디디는 어릴 때나 지금이나 너무 사랑스러워요! * 크집사인스타 ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/eE-APRkDBO0/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/eE-APRkDBO0/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/eE-APRkDBO0/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: '크림히어로즈',
+      liveBroadcastContent: 'none',
+      publishTime: '2018-12-20T03:00:02Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'GVlCSi-crLimP3F4KQ7f1EPGly0',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'YigJW50h8Jw',
+    },
+    snippet: {
+      publishedAt: '2022-01-05T02:11:54Z',
+      channelId: 'UCKwucPzHZ7zCUIf7If-Wo1g',
+      title:
+        'वार्ता: पीएम मोदी का पंजाब दौरा आज व संस्कृत में देखिए अन्य समाचार',
+      description:
+        "DD News is India's 24x7 news channel from the stable of the country's Public Service Broadcaster, Prasar Bharati. It has the ...",
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/YigJW50h8Jw/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/YigJW50h8Jw/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/YigJW50h8Jw/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'DD News',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-01-05T02:11:54Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'rglMup1qkckHBFdvI1S3dm4MOME',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'QmuSm1Ltt2c',
+    },
+    snippet: {
+      publishedAt: '2021-10-31T04:00:14Z',
+      channelId: 'UClFXUaeoTK0mAHx7aAd5MAg',
+      title:
+        'dd噪台北Vlog｜原來北車附近有這些景點可以去？入住啤酒主題房間超Chill！',
+      description:
+        '台北車站#路境行旅#北車景點大家應該跟我一樣台北玩到每次朋友問你台北可以去哪？ 你都只能回答不知道了吧   沒錯，我又找到 ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/QmuSm1Ltt2c/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/QmuSm1Ltt2c/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/QmuSm1Ltt2c/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'dd邊走邊吃',
+      liveBroadcastContent: 'none',
+      publishTime: '2021-10-31T04:00:14Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'SlYNZmPCaTo5D6QVxUKQd2sEOBw',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'SVgqQ0cqHHY',
+    },
+    snippet: {
+      publishedAt: '2022-01-05T03:34:19Z',
+      channelId: 'UCe4GErYKwpMUJWZpxOiATYA',
+      title: 'DD Bangla Live News at 8:30 AM : 05-01-2022',
+      description: 'সকাল ৮: ৩০ এর ডিডি বাংলার সংবাদ সরাসরি : ০৫-০১-২০২২.',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/SVgqQ0cqHHY/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/SVgqQ0cqHHY/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/SVgqQ0cqHHY/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'DD Bangla News',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-01-05T03:34:19Z',
+    },
+  },
+];
+
+export default VIDEOS;
